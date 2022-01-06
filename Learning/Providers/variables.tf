5b77fd7dcbd28cf2ef053e47a7359ef3fbb8fbee
Dom <dominic@booandjoel.co.uk>
2022-01-06 21:45:18 +0000

    Added  variable types

--- Learning/Providers/variables.tf
+++ Learning/Providers/variables.tf
@@ -1,18 +1,22 @@
 variable "location" {
     description = "Location name"
     default = "North Europe"
+    type = string
 }
 variable "subscriptionId" {
     description = "Subscription Id"
-    default = ""  
+    default = ""
+    type = string 
 }
 variable "environment" {
     description = "Environment: Dev, Test, Prod..."
     default = "Dev"
+    type = string
 }
 variable "usage" {
     description = "The resource group usage - application or infrastructure"
     default = "usage"
+    type = string
 }
 variable "tags" {
     description = "Tags required for the resource group"
