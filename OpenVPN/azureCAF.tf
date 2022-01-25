852106f44597d60edee83e246ca736b156be7936
Dom Clayton <dominic@booandjoel.co.uk>
2022-01-25 09:28:40 +0000

    Updated CAF to use new random resource results

--- OpenVPN/azureCAF.tf
+++ OpenVPN/azureCAF.tf
@@ -1,27 +1,27 @@
 resource "azurecaf_name" "resourceGroup" {
-    name = lower(random_id.resourceGroup.id)
+    name = lower(random_id.subscriptionAndEnvironmentAndLocationUnique.id)
     resource_type = "azurerm_resource_group"
     suffixes = [ var.usage ]
 }
 
 resource "azurecaf_name" "networkWatcher" {
-    name = lower(random_id.networkWatcher.id)
+    name = lower(random_id.subscriptionAndLocationUnique.id)
     resource_type = "azurerm_network_watcher"
 }
 
 resource "azurecaf_name" "virtualNetwork" {
-    name = lower(random_id.virtualNetwork.id)
+    name = lower(random_id.resourceGroupUnique.id)
     resource_type = "azurerm_virtual_network"
     suffixes = ["hub"]
 }
 
 resource "azurecaf_name" "availabilitySet" {
-    name = lower(random_id.availabilitySet)
+    name = lower(random_id.resourceGroupUnique.id)
     resource_type = "azurerm_availability_set"
     suffixes = [ var.usage ]
 }
 
 resource "azurecaf_name" "networkAdapter" {
-    name = lower(random_id.networkAdapter)
-    resource_type = azurerm_network_interface
+    name = lower(random_id.resourceGroupUnique.id)
+    resource_type = "azurerm_network_interface"
 }
