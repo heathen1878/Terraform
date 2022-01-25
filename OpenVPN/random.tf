0a98d7a7ce1ded47c4f931732fe6608cae2e1831
Dom Clayton <dominic@booandjoel.co.uk>
2022-01-25 09:28:20 +0000

    Updated randoms to be more specific

--- OpenVPN/random.tf
+++ OpenVPN/random.tf
@@ -1,4 +1,4 @@
-resource "random_id" "resourceGroup" {
+resource "random_id" "subscriptionAndEnvironmentAndLocationUnique" {
     keepers = {
         environment = var.environment
         location = trimspace( var.location )
@@ -7,32 +7,18 @@ resource "random_id" "resourceGroup" {
     byte_length = 16
 }
 
-resource "random_id" "networkWatcher" {
+resource "random_id" "subscriptionAndLocationUnique" {
     keepers = {
         location = trimspace( var.location )
         subscription = var.subscriptionId
     }
-    byte_length = 32
+    byte_length = 16
 }
 
-resource "random_id" "virtualNetwork" {
+resource "random_id" "resourceGroupUnique" {
     keepers = {
         resourceGroup = azurerm_resource_group.resourceGroup.name
         usage = var.usage
     }
-    byte_length = 32
-}
-
-resource "random_id" "availabilitySet" {
-    keepers = {
-        resourceGroup = azurerm_resource_group.resourceGroup.name
-        usage = var.usage
-    }
-}
-
-resource "random_id" "networkAdapter" {
-    keepers = {
-        resourceGroup = azurerm_resource_group.resourceGroup.name
-        usage = var.usage
-    }
+    byte_length = 16
 }
