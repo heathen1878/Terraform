38e3bfedbbdfbb60c13f89a2733582988f5a2912
Dom Clayton <dominic@booandjoel.co.uk>
2022-01-28 16:43:05 +0000

    updated rbac

--- OpenVPN/rbac.tf
+++ OpenVPN/rbac.tf
@@ -2,25 +2,16 @@ resource "azurerm_role_assignment" "keyVaultAdmin" {
   scope = azurerm_key_vault.keyVault.id
   role_definition_name = "Key Vault Administrator"
   principal_id = var.keyVaultAdmin
-  depends_on = [
-      azurerm_key_vault.keyVault
-  ]
 }
 
 resource "azurerm_role_assignment" "keyVaultSecretsOfficer" {
   scope = azurerm_key_vault.keyVault.id
   role_definition_name = "Key Vault Secrets Officer"
   principal_id = var.keyVaultSecretsOfficer
-  depends_on = [
-      azurerm_key_vault.keyVault
-  ]
 }
 
 resource "azurerm_role_assignment" "keyVaultCertificatesOfficer" {
   scope = azurerm_key_vault.keyVault.id
   role_definition_name = "Key Vault Certificates Officer"
   principal_id = var.keyVaultCertificatesOfficer
-  depends_on = [
-      azurerm_key_vault.keyVault
-  ]
 }
