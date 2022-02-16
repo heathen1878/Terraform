b2cf0eb459f538def78c7ee6d23dfe936470e466
Dom Clayton <dominic@booandjoel.co.uk>
2022-02-16 14:53:35 +0000

    Initial code to build a vNet Gateway

---
+++ vNetGateway/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_version = "~> 0.14.0"
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 2.89.0"
+        }
+        azuread = {
+            source = "hashicorp/azuread"
+            version = "~> 2.18.0"
+        }
+        random = {
+
+        }
+        azurecaf = {
+            source = "aztfmod/azurecaf"
+            version = "~> 1.2.10"
+        }
+    }
+}
