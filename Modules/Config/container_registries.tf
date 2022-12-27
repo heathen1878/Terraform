4acf2bd5a512f0b9cd32eb4ec0a926b4c314b041
Dom Clayton <dominic@booandjoel.co.uk>
2022-12-27 22:09:41 +0000

    Code updates for acr

---
+++ Modules/Config/container_registries.tf
@@ -0,0 +1,49 @@
+locals {
+
+  container_registries = {
+    northeuropeacr = {
+    }
+    northeuropewesteuropeacr = {
+      sku = "Premium"
+      georeplications = {
+        west_europe = {
+          location                  = "west europe"
+          regional_endpoint_enabled = true
+          zone_redundancy_enabled   = true
+          tags = {
+            enabled  = "True"
+            Location = "West Europe"
+          }
+        }
+        uk_south = {
+          location                  = "uk south"
+          regional_endpoint_enabled = true
+          zone_redundancy_enabled   = true
+          tags = {
+            enabled  = "True"
+            Location = "UK South"
+          }
+        }
+      }
+    }
+  }
+
+  # ---------------------------------------------------------------------------------------------------------------------
+  # LOCAL CALCULATED
+  # ---------------------------------------------------------------------------------------------------------------------
+
+  container_registry_output = {
+    for acr_key, acr_value in local.container_registries : acr_key => {
+      name                       = azurecaf_name.acr[acr_key].result
+      resource_group             = lookup(acr_value, "resource_group", "demo")
+      sku                        = lookup(acr_value, "sku", "Basic")
+      admin_enabled              = lookup(acr_value, "admin_enabled", false)
+      anonymous_pull_enabled     = lookup(acr_value, "anonymous_pull_enabled", false)
+      data_endpoint_enabled      = lookup(acr_value, "data_endpoint_enabled", false)
+      georeplications            = lookup(acr_value, "georeplications", {})
+      network_rule_bypass_option = lookup(acr_value, "network_rule_bypass_option", "AzureServices")
+    }
+  }
+
+
+}
