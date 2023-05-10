1ea4a199b090466211f7d57987ecc2c01713c8a5
Dom Clayton <dominic@booandjoel.co.uk>
2023-05-10 09:40:19 +0100

    Code updates for RG module

--- modules/terraform-azure-resource-groups/resource_group.tf
+++ modules/terraform-azure-resource-groups/resource_group.tf
@@ -6,14 +6,3 @@ resource "azurerm_resource_group" "resource_group" {
   tags = each.value.tags
 
 }
-
-locals {
-
-  resource_group_outputs = {
-    for resource_group_key, resource_group_value in azurerm_resource_group.resource_group : resource_group_key => {
-      name     = resource_group_value.name
-      location = resource_group_value.location
-    }
-  }
-
-}
