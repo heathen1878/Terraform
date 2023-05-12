f2bbb387670e1ac24ed487c7b157ecca04a932aa
Dom Clayton <dominic@booandjoel.co.uk>
2023-05-12 13:56:04 +0100

    housekeeping

--- root_modules/cloudflare/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "config" {
-    backend = "azurerm"
-
-    config = {
-      storage_account_name = "sthn37mgfywa7g4"
-        container_name       = format()
-        key                  = "config.tfstate"
-    }
-}
