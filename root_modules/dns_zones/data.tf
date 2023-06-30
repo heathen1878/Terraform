bb5e97f5976206cff0148a745f09f667a25a67ee
Dom Clayton <dominic@booandjoel.co.uk>
2023-06-30 14:29:04 +0100

    environment DNS zones root module

--- root_modules/dns_zones/data.tf
+++ root_modules/dns_zones/data.tf
@@ -8,3 +8,25 @@ data "terraform_remote_state" "config" {
   }
 
 }
+
+data "terraform_remote_state" "infrastructure" {
+  backend = "azurerm"
+
+  config = {
+    storage_account_name = var.state_storage_account
+    container_name       = format("%s-%s-%s", var.namespace, var.environment, var.location)
+    key                  = "infrastructure.tfstate"
+  }
+
+}
+
+data "terraform_remote_state" "global_dns_zones" {
+  backend = "azurerm"
+
+  config = {
+    storage_account_name = var.state_storage_account
+    container_name       = format("%s-%s", var.tenant_id, var.location)
+    key                  = "global_dns_zones.tfstate"
+  }
+
+}
--- root_modules/dns_zones/main.tf
+++ root_modules/dns_zones/main.tf
@@ -1,6 +1,6 @@
 module "dns" {
   source  = "heathen1878/dns/azurerm"
-  version = "1.0.0"
+  version = "1.0.1"
 
   private_dns_zones = local.private_dns_zones
   public_dns_zones  = local.public_dns_zones
@@ -11,14 +11,22 @@ locals {
   private_dns_zones = {
     for key, value in data.terraform_remote_state.config.outputs.dns.private_dns_zones : key => {
       name                 = value.name
-      resource_group_name  = module.resource_groups.resource_group[value.resource_group].name
+      resource_group_name  = data.terraform_remote_state.infrastructure.outputs.resource_groups[value.resource_group].name
       tags                 = value.tags
-      virtual_network_name = module.networking.virtual_network["environment"].name
-      virtual_network_id   = module.networking.virtual_network["environment"].id
+      virtual_network_name = data.terraform_remote_state.infrastructure.outputs.virtual_networks["environment"].name
+      virtual_network_id   = data.terraform_remote_state.infrastructure.outputs.virtual_networks["environment"].id
     }
   }
 
-  # TODO: if there are azure managed dns zones then add subdomains for environments other than production.
-  public_dns_zones = {}
+  public_dns_zones = {
+    for key, value in data.terraform_remote_state.config.outputs.dns.subdomains : key => {
+      name                          = data.terraform_remote_state.global_dns_zones.outputs.dns.zones.azure[value.name].name
+      resource_group_name           = data.terraform_remote_state.global_dns_zones.outputs.dns.zones.azure[value.name].resource_group_name
+      subdomain                     = value.subdomain
+      subdomain_resource_group_name = data.terraform_remote_state.infrastructure.outputs.resource_groups[value.resource_group].name
+      ttl                           = value.ttl
+      tags                          = value.tags
+    }
+  }
 
 }
